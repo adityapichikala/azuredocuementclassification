a5b006c7547089cf636f587037be6dda4e5f2b16
adityapichikala <adityapichikala@gmail.com>
2025-12-22 22:01:01 +0530

    Clean up non-essential files: remove logs, test scripts, and build artifacts

--- DocumentClassificationProject/create_pdf.py
+++
@@ -1,5 +0,0 @@
-def create_pdf(filename):
-    with open(filename, 'wb') as f:
-        f.write(b'%PDF-1.4\n1 0 obj\n<<\n/Type /Catalog\n/Pages 2 0 R\n>>\nendobj\n2 0 obj\n<<\n/Type /Pages\n/Kids [3 0 R]\n/Count 1\n>>\nendobj\n3 0 obj\n<<\n/Type /Page\n/Parent 2 0 R\n/MediaBox [0 0 612 792]\n/Resources << >>\n>>\nendobj\nxref\n0 4\n0000000000 65535 f \n0000000010 00000 n \n0000000060 00000 n \n0000000117 00000 n \ntrailer\n<<\n/Size 4\n/Root 1 0 R\n>>\nstartxref\n223\n%%EOF')
-
-create_pdf('test_trigger.pdf')
